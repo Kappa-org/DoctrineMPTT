892d9251b7fccece5206410806ce3ae72ad57def
Budry <zarubaondra@gmail.com>
2015-02-05 19:11:13 +0100

    Added MissingConfigurator exceptions

---
+++ src/exceptions.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * This file is part of the Kappa\DoctrineMPTT package.
+ *
+ * (c) Ondřej Záruba <zarubaondra@gmail.com>
+ *
+ * For the full copyright and license information, please view the license.md
+ * file that was distributed with this source code.
+ */
+
+namespace Kappa\DoctrineMPTT;
+
+/**
+ * Class MissingConfiguratorException
+ *
+ * @package Kappa\DoctrineMPTT
+ * @author Ondřej Záruba <http://zaruba-ondrej.cz>
+ */
+class MissingConfiguratorException extends \LogicException
+{
+
+}
