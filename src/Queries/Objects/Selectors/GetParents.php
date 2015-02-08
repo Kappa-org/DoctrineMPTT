6ca9d547e1452ef669a728b9fc4f43f790d3fc3c
Budry <zarubaondra@gmail.com>
2015-02-08 20:08:30 +0100

    Added GetParents QueryObject

---
+++ src/Queries/Objects/Selectors/GetParents.php
@@ -0,0 +1,61 @@
+<?php
+/**
+ * This file is part of the Kappa\DoctrineMPTT package.
+ *
+ * (c) Ondřej Záruba <zarubaondra@gmail.com>
+ *
+ * For the full copyright and license information, please view the license.md
+ * file that was distributed with this source code.
+ */
+
+namespace Kappa\DoctrineMPTT\Queries\Objects\Selectors;
+
+use Kappa\DoctrineMPTT\Configurator;
+use Kappa\DoctrineMPTT\Entities\TraversableInterface;
+use Kappa\DoctrineMPTT\Utils\StringComposer;
+use Kdyby;
+use Kdyby\Doctrine\QueryObject;
+
+/**
+ * Class GetParents
+ *
+ * @package Kappa\DoctrineMPTT\Queries\Selectors
+ * @author Ondřej Záruba <http://zaruba-ondrej.cz>
+ */
+class GetParents extends QueryObject
+{
+	/** @var Configurator */
+	private $configurator;
+
+	/** @var TraversableInterface */
+	private $actual;
+
+	/**
+	 * @param Configurator $configurator
+	 * @param TraversableInterface $actual
+	 */
+	public function __construct(Configurator $configurator, TraversableInterface $actual)
+	{
+		$this->actual = $actual;
+		$this->configurator = $configurator;
+	}
+
+	/**
+	 * @param \Kdyby\Persistence\Queryable $repository
+	 * @return \Doctrine\ORM\Query|\Doctrine\ORM\QueryBuilder
+	 */
+	protected function doCreateQuery(Kdyby\Persistence\Queryable $repository)
+	{
+		$stringComposer = new StringComposer([
+			':leftName:' => $this->configurator->get(Configurator::LEFT_NAME),
+			':rightName:' => $this->configurator->get(Configurator::RIGHT_NAME)
+		]);
+		$queryBuilder = $repository->createQueryBuilder('e')
+			->select('e')
+			->where($stringComposer->compose('e.:leftName: < ?0'))
+			->andWhere($stringComposer->compose('e.:rightName: > ?1'))
+			->setParameters([$this->actual->getLeft(), $this->actual->getRight()]);
+
+		return $queryBuilder;
+	}
+}
---
+++ tests/KappaTests/DoctrineMPTT/Queries/Selectors/GetParentsTest.phpt
@@ -0,0 +1,55 @@
+<?php
+/**
+ * This file is part of the Kappa\DoctrineMPTT package.
+ *
+ * (c) Ondřej Záruba <zarubaondra@gmail.com>
+ *
+ * For the full copyright and license information, please view the license.md
+ * file that was distributed with this source code.
+ *
+ * @testCase
+ */
+
+namespace Kappa\DoctrineMPTT\Tests;
+
+use Kappa\DoctrineMPTT\Queries\Objects\Selectors\GetParents;
+use KappaTests\DoctrineMPTT\Mocks\Entity;
+use KappaTests\DoctrineMPTT\ORMTestCase;
+use KappaTests\Mocks\GetAllQueryObject;
+use Tester\Assert;
+use Tester\Environment;
+
+require_once __DIR__ . '/../../../bootstrap.php';
+
+/**
+ * Class GetParentsTest
+ *
+ * @package Kappa\DoctrineMPTT\Tests
+ * @author Ondřej Záruba <http://zaruba-ondrej.cz>
+ */
+class GetParentsTest extends ORMTestCase
+{
+	/** @var \Kappa\DoctrineMPTT\Configurator */
+	private $configurator;
+
+	protected function setUp()
+	{
+		parent::setUp();
+		$this->configurator = $this->container->getByType('Kappa\DoctrineMPTT\Configurator');
+	}
+
+	public function testGetParents()
+	{
+		$actual = $this->repository->find(9);
+		$expected = [
+			Entity::createWithId(1, 1, 18, 0),
+			Entity::createWithId(3, 6, 15, 1),
+			Entity::createWithId(8, 11, 14, 2),
+		];
+		Assert::equal($expected, $this->repository->fetch(new GetParents($this->configurator, $actual))->toArray());
+	}
+}
+
+Environment::lock("database", dirname(TEMP_DIR));
+
+\run(new GetParentsTest());
