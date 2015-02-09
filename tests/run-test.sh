a3d45419ee9f48536c7f877412ccfd9872dadf82
Budry <zarubaondra@gmail.com>
2015-02-09 09:11:59 +0100

    Added error test logs

--- tests/run-test.sh
+++ tests/run-test.sh
@@ -4,8 +4,11 @@ BASEDIR=$(dirname $0)
 
 $BASEDIR/../vendor/bin/tester $BASEDIR -c $BASEDIR/php-unix.ini
 
+result=$?
 
-
+echo ""
+echo "=================================================="
+echo "SQL LOGS"
 for entry in "$BASEDIR/logs"/*
 do
   echo "=================================================="
@@ -13,5 +16,14 @@ do
   echo "--------------------------------------------------"
   cat $entry
 done
+
+if  [ $result = 1 ]; then
+  echo ""
+  for i in $(find ./tests/ -name *.actual -o -name *.expected); do
+    echo "---- $i"
+    cat $i
+    echo "=================================================="
+  done
+fi
 
 
